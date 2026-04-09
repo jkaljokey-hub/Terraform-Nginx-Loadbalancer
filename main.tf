3e5aa4d856141ca1da60fb56916cfca5cf5ebc7c
abubakar <jkaljokey@gmail.com>
2026-04-09 22:03:36 +0300

    myNG

--- main.tf
+++ main.tf
@@ -13,6 +13,9 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
+provider "azurerm" {
+  features {}
+}
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
